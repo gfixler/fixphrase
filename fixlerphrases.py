774242fe560a225c1a59c03638fbb716c67bb7f8
gfixler <gary@garyfixler.com>
2022-08-12 02:10:25 -0700

    Begin fixlerphrases.py
    I created some irregular verb groups using info from this page:
    https://www.wallstreetenglish.com/blog/regular-and-irregular-english-verbs
    
    These irregular groups can be used to generate strokes from minimal
    information, e.g. for some verbs, all forms are the same, so those are
    just in a list of single words, representing all forms.

---
+++ fixlerphrases.py
@@ -0,0 +1,78 @@
+"""
+note: verb "to be" has 8 forms and is handled separately
+"""
+
+"""
+Irregular verbs group 1
+same base form, past simple, and past participle
+"""
+irreg1 = [
+    "cost",
+    "cut",
+    "hurt",
+    "let",
+    "put",
+]
+
+"""
+Irregular verbs group 2
+same past simple and past participle (omitted)
+different base form
+"""
+irreg2 = [
+    ("bring", "brought"),
+    ("buy", "bought"),
+    ("catch", "caught"),
+    ("feel", "felt"),
+    ("find", "found"),
+    ("get", "got"),
+    ("have", "had"),
+    ("hear", "heard"),
+    ("keep", "kept"),
+    ("leave", "left"),
+    ("lose", "lost"),
+    ("make", "made"),
+    ("read", "read"),
+    ("say", "said"),
+    ("sell", "sold"),
+    ("send", "sent"),
+    ("teach", "taught"),
+    ("think", "thought"),
+    ("win", "won"),
+]
+
+"""
+Irregular verbs group 3
+same base form and past participle (omitted)
+different past simple
+"""
+irreg3 = [
+    ("come", "came"),
+    ("become", "became"),
+    ("run", "ran"),
+]
+
+"""
+Irregular verbs group 4
+different base form, past simple, past participle
+"""
+irreg4 = [
+    ("begin", "began", "begun"),
+    ("break", "broke", "broken"),
+    ("choose", "chose", "chosen"),
+    ("do", "did", "done"),
+    ("drink", "drank", "drunk"),
+    ("drive", "drove", "driven"),
+    ("eat", "ate", "eaten"),
+    ("fall", "fell", "fallen"),
+    ("give", "gave", "given"),
+    ("go", "went", "gone"),
+    ("know", "knew", "known"),
+    ("see", "saw", "seen"),
+    ("speak", "spoke", "spoken"),
+    ("swim", "swam", "swum"),
+    ("take", "took", "taken"),
+    ("wake", "woke", "woken"),
+    ("write", "wrote", "written"),
+]
+
